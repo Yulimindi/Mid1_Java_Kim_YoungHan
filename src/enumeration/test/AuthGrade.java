c789b94262e850520d6caf69ed05c3a03c274e83
Yulimindi <128598263+Yulimindi@users.noreply.github.com>
2025-09-22 16:02:37 +0900

    250922/~섹션6. 열거형-ENUM까지

---
+++ src/enumeration/test/AuthGrade.java
@@ -0,0 +1,25 @@
+package enumeration.test;
+
+public enum AuthGrade {
+    GUEST(1, "손님"),
+    LOGIN(2, "로그인 회원"),
+    ADMIN(3, "관리자");
+
+    private final int level;
+    private final String description;
+
+    AuthGrade(int level, String description) {
+        this.level = level;
+        this.description = description;
+    }
+
+    // level과 description을 조회할 수 있어야 한다 = getter 사용해라
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/enumeration/test/AuthGradeMain2.java
@@ -0,0 +1,27 @@
+package enumeration.test;
+import enumeration.ex3.Grade;
+
+import java.util.Scanner;
+
+public class AuthGradeMain2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("당신의 등급을 입력하세요 [GUEST, LOGIN, ADMIN] : ");
+        String grade = scanner.nextLine();
+
+        AuthGrade authGrade = AuthGrade.valueOf(grade.toUpperCase()); // 뭐가 입력되어도 대문자로 바꿈 -> 사용자 친화적
+        System.out.println("당신의 등급은 " + authGrade.getDescription() + "입니다.");
+
+        System.out.println(" == 메뉴 목록 == ");
+        if (authGrade.getLevel() > 0) {
+            System.out.println("- 메인 화면");
+        }
+        if (authGrade.getLevel() > 1) {
+            System.out.println("- 이메일 관리 화면");
+        }
+        if (authGrade.getLevel() > 2) {
+            System.out.println("- 관리자 화면");
+        }
+    }
+
+}
